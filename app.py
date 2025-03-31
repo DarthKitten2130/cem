7d4be1f494572a4748d854b1cf6043b33a67ff6a
DarthKitten <darthkitten2228@gmail.com>
2025-03-31 22:05:19 +0530

    added acc verification

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for,session
 from sql import *
 
 app = Flask(__name__)
--- sql.py
+++ sql.py
@@ -34,3 +34,25 @@ def get_user(id):
     ''')
     user = cursor.fetchone()
     return user
+
+
+def account_verification(username, password):
+    global cursor
+    cursor.execute(
+        f'SELECT username,password from users where username = "{username}"')
+    results = cursor.fetchall()
+    acc = {}
+    for result in results:
+        acc[result[0]] = result[1]
+
+    # Account Does Not Exist
+    if username not in [x[0] for x in results]:
+        return 'doesNotExist'
+
+    # Entered the Wrong Password
+    elif acc[username] != password:
+        return 'wrongPassword'
+
+    # Verified
+    elif acc[username] == password:
+        return 'verified'
