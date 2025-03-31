9ff62af5a848e81ad40c7b8019131bb9ab7c3340
DarthKitten <darthkitten2228@gmail.com>
2025-03-31 16:24:22 +0530

    fixed signin

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
-import sqlite3 as sql
+from sql import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    create_table()
     return render_template('index.html')
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    create_table()
+    if request.method == 'POST':
+        id = request.form['id']
+        password = request.form['password']
+
+        user = get_user(id)
+        if user and user[5] == password:
+            return redirect(url_for('home'))
+        else:
+            return render_template('signin.html', error='Invalid credentials')
+
+    return render_template('signin.html')
+
+
+@app.route('/createaccount', methods=['GET', 'POST'])
+def create_account():
+    create_table()
+    if request.method == 'POST':
+        id = request.form['id']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        branch = request.form['branch']
+        password = request.form['password']
+
+        insert_user(id, name, email, phone, branch, password)
+
+        return redirect(url_for('home'))
+
+    return render_template('createaccount.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ sql.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+sqliteConnection = conn = sqlite3.connect('sql.db', check_same_thread=False)
+cursor = sqliteConnection.cursor()
+
+
+def create_table():
+    # Create a table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id TEXT PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            phone TEXT NOT NULL,
+            branch TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    sqliteConnection.commit()
+
+
+def insert_user(id, name, email, phone, branch, password):
+    # Insert a user
+    cursor.execute(f'''
+        INSERT INTO users (id, name, email, phone, branch, password)
+        VALUES ('{id}','{name}', '{email}', '{phone}', '{branch}', '{password}')''')
+    sqliteConnection.commit()
+
+
+def get_user(id):
+    # Get a user
+    cursor.execute(f'''
+        SELECT * FROM users WHERE id = {id}
+    ''')
+    user = cursor.fetchone()
+    return user
